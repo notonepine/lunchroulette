82bb1daab9f885a0ae6cb0b94bccd3408a7c4046
Vicv <victorvuc@gmail.com>
2014-01-25 02:20:59 -0800

    Fixed startup crash

--- src/com/notonepine/lunchroulette/LoginFragment.java
+++ src/com/notonepine/lunchroulette/LoginFragment.java
@@ -1,6 +1,6 @@
 package com.notonepine.lunchroulette;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +10,8 @@ public class LoginFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        inflater.inflate(R.layout.fragment_login, null);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
     }
 
 }

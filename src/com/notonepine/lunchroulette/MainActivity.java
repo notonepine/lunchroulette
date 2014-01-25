d1922004690c9add80b683a384a4ba154e0baa3a
Vicv <victorvuc@gmail.com>
2014-01-25 02:03:16 -0800

    aaa

--- src/com/notonepine/lunchroulette/MainActivity.java
+++ src/com/notonepine/lunchroulette/MainActivity.java
@@ -2,7 +2,6 @@ package com.notonepine.lunchroulette;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
 
 public class MainActivity extends FragmentActivity {
 

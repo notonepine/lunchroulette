bacc97c9495f77f231e5dcef7b0b707b44c6cd0e
Dustin Lam <mynameisdustinlam@gmail.com>
2014-01-25 20:49:21 -0800

    Import Google Play Services lib properly.

--- src/com/notonepine/lunchroulette/Utils.java
+++ src/com/notonepine/lunchroulette/Utils.java
@@ -1,9 +1,7 @@
 package com.notonepine.lunchroulette;
 
-import com.facebook.model.GraphUser;
-
 public class Utils {
-    public static String getUserAvatarUrl(String userId) {
-        return "http://graph.facebook.com/" + userId + "/picture?type=large";
-    }
+	public static String getUserAvatarUrl(String userId) {
+		return "http://graph.facebook.com/" + userId + "/picture?type=large";
+	}
 }

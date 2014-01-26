6e33989c6b4bf8d4f6d2a60b28ab63c24294482f
Josh Dover <gerfuls@gmail.com>
2014-01-26 04:27:53 -0800

    Polling server!

--- src/com/notonepine/lunchroulette/FinalFragment.java
+++ src/com/notonepine/lunchroulette/FinalFragment.java
@@ -1,5 +1,12 @@
 package com.notonepine.lunchroulette;
 
+import org.json.JSONObject;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,6 +20,7 @@ import android.widget.LinearLayout;
 
 public class FinalFragment extends Fragment {
     View mView;
+    SharedPreferences mPrefs;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +32,11 @@ public class FinalFragment extends Fragment {
                 animateSomeStuff(((LinearLayout) mView.findViewById(R.id.person_one_layout)), 600);
             }
         });
+
+        mPrefs = getActivity().getSharedPreferences("com.notonepine.lunchroulette", Context.MODE_PRIVATE);
+        String userId = mPrefs.getString(LunchRouletteFragmentActivity.USER_ID, "");
+        Location location = Utils.getLocation(getActivity());
+        NetworkUtils.beginSearch(userId, location.getLatitude(), location.getLongitude(), newUserFound(), locationFound());
         return mView;
     }
 
@@ -36,4 +49,26 @@ public class FinalFragment extends Fragment {
         view.startAnimation(anim);
     }
 
+    private JsonHttpResponseHandler newUserFound() {
+    	return new JsonHttpResponseHandler() {
+    		@Override
+    		public void onSuccess(JSONObject userData) {
+    			String name = userData.optString("name");
+    			String avatarUrl = Utils.getUserAvatarUrl(userData.optString("fb_id"));
+    			// do something with this
+    		}
+    	};
+    }
+
+    private JsonHttpResponseHandler locationFound() {
+    	return new JsonHttpResponseHandler() {
+    		@Override
+    		public void onSuccess(JSONObject restaurant) {
+    			double latitude = restaurant.optDouble("lat");
+    			double longitude = restaurant.optDouble("long");
+    			String name = restaurant.optString("name");
+    			// do something with this
+    		}
+    	};
+    }
 }
